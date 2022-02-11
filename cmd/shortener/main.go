fe555c4142910015e8b37c398760e39eea5e795b
naneri <ktnaneri@gmail.com>
2022-02-11 16:13:25 +0600

    added a get handler

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,46 +2,54 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
-	"os"
 )
 
+type mainRequest struct {
+	url string
+}
+
+var storage map[int]string
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
+			return
+		}
+
+		w.WriteHeader(http.StatusTemporaryRedirect)
+		w.Write([]byte("http://yandex.ru"))
+		return
+	}
 
 	if r.Method != http.MethodPost {
 		http.Error(w, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
 		return
 	}
 
-	b, err := io.ReadAll(r.Body)
-	// обрабатываем ошибку
-	if err != nil {
-		http.Error(w, err.Error(), 500)
+	if err := r.ParseForm(); err != nil {
+		// если не заполнена, возвращаем код ошибки
+		http.Error(w, "Bad request", 401)
 		return
 	}
 
-	fmt.Println(b)
-	os.Exit(0)
-	//urlToShorten := r.URL.Query().Get("url")
-	//if urlToShorten == "" {
-	//    http.Error(w, "The url parameter is missing", http.StatusBadRequest)
-	//    return
-	//}
-	w.Header().Set("content-type", "application/json")
+	w.Header().Set("content-type", "plain/text")
 	w.WriteHeader(http.StatusCreated)
 
 	responseJson := map[string]string{
 		"id":           "1",
 		"shortenedUrl": "localhost:8080/1",
 	}
-	fmt.Println(responseJson)
+	w.Write([]byte(responseJson["shortenedUrl"]))
 }
 
 func main() {
 
 	// маршрутизация запросов обработчику
 	http.HandleFunc("/", indexHandler)
+	//http.HandleFunc()
 	// запуск сервера с адресом localhost, порт 8080
 
 	fmt.Println("starting the server")
