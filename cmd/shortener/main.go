ae745bbc50528da36cb31a7ae821f122e96a33e3
naneri <ktnaneri@gmail.com>
2022-02-11 14:13:55 +0600

    fixed the compilation

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,37 +2,48 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodPost {
-        http.Error(w, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
-        return
-    }
+		http.Error(w, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
+		return
+	}
 
-    urlToShorten := r.URL.Query().Get("url")
-    if urlToShorten == "" {
-        http.Error(w, "The url parameter is missing", http.StatusBadRequest)
-        return
-    }
+	b, err := io.ReadAll(r.Body)
+	// обрабатываем ошибку
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	fmt.Println(b)
+	os.Exit(0)
+	//urlToShorten := r.URL.Query().Get("url")
+	//if urlToShorten == "" {
+	//    http.Error(w, "The url parameter is missing", http.StatusBadRequest)
+	//    return
+	//}
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 
 	responseJson := map[string]string{
-		"id" : "1",
-		"shortenedUrl" : "localhost:8080/1",
+		"id":           "1",
+		"shortenedUrl": "localhost:8080/1",
 	}
-	fmt.Println(urlToShorten)
+	fmt.Println(responseJson)
 }
 
 func main() {
 
-	 // маршрутизация запросов обработчику
-	 http.HandleFunc("/", indexHandler)
-	 // запуск сервера с адресом localhost, порт 8080
+	// маршрутизация запросов обработчику
+	http.HandleFunc("/", indexHandler)
+	// запуск сервера с адресом localhost, порт 8080
 
-	 fmt.Println("starting the server")
-	 http.ListenAndServe(":8080", nil)
+	fmt.Println("starting the server")
+	http.ListenAndServe(":8080", nil)
 }
