c081be5b2083f79b67304230d3e460a440b4ba8a
Kanat Tabaldiev <ktnaneri@gmail.com>
2022-02-13 12:45:09 +0600

    updated for the third increment

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,49 +2,106 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
 	"io"
 	"net/http"
 	"strconv"
-	"strings"
 )
-
-type mainRequest struct {
-	url string
-}
 
 var lastUrlId int
 var storage map[string]string
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		fmt.Printf("GET request to %s \n", r.URL)
-		if r.Method != http.MethodGet {
-			http.Error(w, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
-			return
-		}
+//func indexHandler(w http.ResponseWriter, r *http.Request) {
+//	if r.URL.Path != "/" {
+//		fmt.Printf("GET request to %s \n", r.URL)
+//		if r.Method != http.MethodGet {
+//			http.Error(w, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
+//			return
+//		}
+//
+//		split := strings.Split(r.URL.Path, "/")
+//		id := split[1]
+//
+//		if val, ok := storage[id]; ok {
+//			w.Header().Set("Location", val)
+//			w.WriteHeader(http.StatusTemporaryRedirect)
+//			return
+//		} else {
+//			fmt.Println("URL not found")
+//			http.Error(w, "The URL not found", http.StatusNotFound)
+//			return
+//		}
+//
+//	}
 
-		split := strings.Split(r.URL.Path, "/")
-		id := split[1]
+//if r.Method != http.MethodPost {
+//	http.Error(w, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
+//	return
+//}
+//
+//fmt.Printf("POST request to %s \n", r.URL)
+//
+//body, err := io.ReadAll(r.Body)
+//// обрабатываем ошибку
+//if err != nil {
+//	http.Error(w, err.Error(), 500)
+//	return
+//}
+//
+//w.Header().Set("content-type", "plain/text")
+//w.WriteHeader(http.StatusCreated)
+//
+//lastUrlId++
+//if storage == nil {
+//	storage = make(map[string]string)
+//}
+//storage[strconv.Itoa(lastUrlId)] = string(body)
+//
+//w.Write([]byte(fmt.Sprintf("http://localhost:8080/%d", lastUrlId)))
+//}
 
-		if val, ok := storage[id]; ok {
-			w.Header().Set("Location", val)
-			w.WriteHeader(http.StatusTemporaryRedirect)
-			return
-		} else {
-			fmt.Println("URL not found")
-			http.Error(w, "The URL not found", http.StatusNotFound)
-			return
-		}
+//func getUrl(c *gin.Context) {
+//
+//}
+//
+//
+//func postUrl(c *gin.Context) {
+//	body, err := io.ReadAll(c.Request.Body)
+//
+//	if err != nil {
+//		c.String(http.StatusInternalServerError, err.Error())
+//		return
+//	}
+//
+//	lastUrlId++
+//	if storage == nil {
+//		storage = make(map[string]string)
+//	}
+//	storage[strconv.Itoa(lastUrlId)] = string(body)
+//
+//	c.String(http.StatusCreated, fmt.Sprintf("http://localhost:8080/%d", lastUrlId))
+//}
+func getUrl(w http.ResponseWriter, r *http.Request) {
+	urlId := chi.URLParam(r, "urlId")
 
-	}
-
-	if r.Method != http.MethodPost {
-		http.Error(w, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
+	if urlId == "" {
+		fmt.Println("URL not found")
+		http.Error(w, "The URL not found", http.StatusNotFound)
 		return
 	}
 
-	fmt.Printf("POST request to %s \n", r.URL)
+	if val, ok := storage[urlId]; ok {
+		w.Header().Set("Location", val)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+		return
+	} else {
+		fmt.Println("URL not found")
+		http.Error(w, "The URL not found", http.StatusNotFound)
+		return
+	}
+}
 
+func postUrl(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	// обрабатываем ошибку
 	if err != nil {
@@ -61,15 +118,21 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	storage[strconv.Itoa(lastUrlId)] = string(body)
 
-	w.Write([]byte(fmt.Sprintf("http://localhost:8080/%d", lastUrlId)))
+	_, _ = w.Write([]byte(fmt.Sprintf("http://localhost:8080/%d", lastUrlId)))
 }
 
 func main() {
-	// маршрутизация запросов обработчику
-	http.HandleFunc("/", indexHandler)
-	//http.HandleFunc()
-	// запуск сервера с адресом localhost, порт 8080
+	r := chi.NewRouter()
 
-	fmt.Println("starting the server")
-	http.ListenAndServe("localhost:8080", nil)
+	r.Post("/", postUrl)
+	r.Get("{url}", getUrl)
+
+	_ = http.ListenAndServe(":8080", r)
+	// маршрутизация запросов обработчику
+	//http.HandleFunc("/", indexHandler)
+	////http.HandleFunc()
+	//// запуск сервера с адресом localhost, порт 8080
+	//
+	//fmt.Println("starting the server")
+	//http.ListenAndServe("localhost:8080", nil)
 }
