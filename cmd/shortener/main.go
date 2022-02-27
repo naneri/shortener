3c331a2e4cd33ae88dff745926fe6468d589c135
Kanat Tabaldiev <ktnaneri@gmail.com>
2022-02-28 00:52:25 +0600

    another possible fix

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -13,7 +13,7 @@ import (
 )
 
 type Config struct {
-	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"8080"`
+	ServerAddress string `env:"SERVER_ADDRESS" envDefault:":8080"`
 	BaseUrl       string `env:"BASE_URL" envDefault:"http://localhost:8080"`
 }
 
