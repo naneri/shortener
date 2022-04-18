9a1f39c6dd90fc578fb3b21f215595116b574f6d
Kanat Tabaldiev <ktnaneri@gmail.com>
2022-04-18 14:22:24 +0600

    removed default value

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -17,7 +17,7 @@ import (
 type Config struct {
 	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:":8080"`
 	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"links.txt"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:""`
 }
 
 var cfg Config
